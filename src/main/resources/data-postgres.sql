604e2a8897baf2510ec8b96732faa6e95a03f9be
mdoroz <michal.doroz@grupawp.pl>
2022-05-10 09:03:08 +0200

    #10 docker-compose ready with empty DB init

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -1,4 +1,4 @@
-INSERT INTO resource (resource_id, role) VALUES (1, 'Premium role');
+/* INSERT INTO resource (resource_id, role) VALUES (1, 'Premium role');
 INSERT INTO resource (resource_id, role) VALUES (2, 'Basic role');
 
 INSERT INTO client (client_id, email, login, password, phone_number) VALUES (1, 'jakismail@gmail.com', 'login1', 'pass1', 123456789);
@@ -8,4 +8,4 @@ INSERT INTO subscription (subscription_id, end_date, start_date, sub_type, resou
 INSERT INTO subscription (subscription_id, end_date, start_date, sub_type, resource_id) VALUES (2, '2022-06-02', '2022-07-30', 'BASIC', 2 );
 
 INSERT INTO account (account_id, client_id, subscription_id) VALUES (1, 1, 1);
-INSERT INTO account (account_id, client_id, subscription_id) VALUES (2, 2, 2);
+INSERT INTO account (account_id, client_id, subscription_id) VALUES (2, 2, 2); */
